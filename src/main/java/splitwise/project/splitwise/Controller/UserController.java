2a01f117c9643db821545974241f6bf421799dea
Shravankumar Janwade <shravanjanwade2252@gmail.com>
2024-04-28 23:02:00 +0530

    User Controller Started

--- src/main/java/splitwise/project/splitwise/Controller/UserController.java
+++ src/main/java/splitwise/project/splitwise/Controller/UserController.java
@@ -1,8 +1,26 @@
 package splitwise.project.splitwise.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import splitwise.project.splitwise.Model.User;
+import splitwise.project.splitwise.Services.UserService;
+
 @RestController
+@RequestMapping("/user")
 public class UserController {
-    
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> getUser(User user) {
+        User savedUser = userService.addUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
 }
