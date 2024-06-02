46c40f6fb7834a7bfdcaeeeffa72af511c875909
Ritesh Rao V <ritheshraov016@gmail.com>
2024-06-02 19:01:20 +0530

    added readme file

--- src/main/resources/balances.js
+++
@@ -1,7 +0,0 @@
-function markAsPaid(event) {
-  event.preventDefault();
-  const link = event.target;
-  const reimbursement = link.closest('.reimbursement');
-  reimbursement.style.display = 'none';
-  alert('Marked as paid');
-}
